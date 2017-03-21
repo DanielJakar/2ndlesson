ea39d3e468e85e45918c72147c7af91e391f9a45
DanielJakar <danieljakar@gmail.com>
2017-03-21 17:53:51 +0200

    Initial commit

---
+++ app/src/main/java/android/course/a2ndlesson/HelloWorld2Activity.java
@@ -0,0 +1,13 @@
+package android.course.a2ndlesson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorld2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world2);
+    }
+}
